ed53f3474013ae0fde305f11a218a161eb933e33
ittk-93 <gaiag5245@gmail.com>
2022-07-16 17:56:11 +0900

    neovim, fishの追加

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dotfiles
+
+## 設定項目
+
+- fish
+- nvim
---
+++ config/fish/config.fish
@@ -0,0 +1,4 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
